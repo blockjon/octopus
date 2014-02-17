2ee0130298b402f57b665df0d5e77320d5082b43
Jonathan Block <jonathan@jonathan-VirtualBox.(none)>
2014-02-17 06:23:11 +0000

    Forgot some files in my last commit.

---
+++ lib/BlockJon/Octopus/DAO/GetModelInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Octopus\DAO;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+interface GetModelInterface 
+{
+    
+    /**
+     * @return \Octopus\Model\AbstractModel
+     */
+    static public function getModel();
+    
+}
