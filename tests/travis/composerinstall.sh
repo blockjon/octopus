8d847a3174ca9e6b9d19861fef29942d1e56f4cc
Jonathan Block <jblock@lyft.com>
2014-11-06 21:20:09 -0800

    Update

--- tests/travis/composerinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-composer_install() {
-    composer install
-    return $?
-}
-
-composer_install > ~/composerinstall.log || ( echo "=== COMPOSER INSTALL FAILED ==="; cat ~/composerinstall.log )
