352868f1d9f90234d7abe83418f66d6dc76c0e34
Jonathan Block <jblock@lyft.com>
2014-11-08 13:17:28 -0800

    Update

--- tests/BlockJon/Tests/Octopus/Strategy/PdoMySqlTest.php
+++ tests/BlockJon/Tests/Octopus/Strategy/PdoMySqlTest.php
@@ -7,29 +7,44 @@ use Octopus\Strategy\PdoMySql,
 
 class PdoMySqlTest extends AbstractStrategyTest
 {
-    
     protected $_dbh;
+    protected $_strategy;
     
-    public function setUp() 
+    public function setUp()
     {
+        parent::setUp();
+        if (!extension_loaded('pdo')) {
+            $this->markTestSkipped("pdo extension not installed. skipping test.");
+            return;
+        }
         $config = BookDao::getConfig('pdomysql');
-        $this->_strategy = new PdoMySql(
-            $config
-        );
+        try {
+            $this->_strategy = new PdoMySql(
+                $config
+            );
+        } catch (\Exception $e) {
+            if ($e->getCode() == 2002) {
+                // Can't connect to mysql.
+                $this->markTestSkipped($e->getMessage());
+                return;
+            }
+        }
         $error_code = $this->_strategy->getPdoHandle()->exec("drop table `" . $config['table'] . "` if exists;");
         $fieldDefinitions = '';
-        foreach($config['columns'] as $field) {
+        foreach ($config['columns'] as $field) {
             $fieldDefinitions .= "`$field` varchar(255), ";
         }
         $fieldDefinitions = rtrim($fieldDefinitions);
         $fieldDefinitions = substr($fieldDefinitions, 0, strlen($fieldDefinitions)-1);
         $sql = "CREATE TABLE `" . $config['table'] . "` ($fieldDefinitions);";
         $error_code = $this->_strategy->getPdoHandle()->exec($sql);
-        parent::setUp();
     }
-    
-    public function tearDown() 
+
+    public function tearDown()
     {
+        if (!extension_loaded('pdo') || $this->getStatus() == \PHPUnit_Runner_BaseTestRunner::STATUS_SKIPPED) {
+            return;
+        }
         $config = BookDao::getConfig('pdomysql');
         $sql = "DROP TABLE `" . $config['table'] . "`;";
         $error_code = $this->_strategy->getPdoHandle()->exec($sql);
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,7 @@ if (!file_exists($file = __DIR__.'/../vendor/autoload.php')) {
     throw new RuntimeException('Install dependencies to run test suite.');
 }
 
-$loader = require_once $file;
+$loader = require $file;
 
 $loader->add('Octopus', __DIR__ . '/../lib/BlockJon');
 $loader->add('BlockJon\Tests', __DIR__ . '/../tests');
@@ -15,3 +15,51 @@ $loader->add('Models', __DIR__);
 $loader->add('Daos', __DIR__);
 $loader->add('Repositories', __DIR__);
 
+// This list of error codes are considered fatal.
+$fatalErrorCodeArray = array(E_USER_ERROR, E_COMPILE_ERROR, E_PARSE, E_ERROR);
+
+/**
+ * Given a PHP error int, return the string representation of the error.
+ *
+ * @param $intval
+ * @param string $separator
+ * @return string
+ */
+$describeErrorReporting = function($intval, $separator = ',') {
+    $errorlevels = array(
+        8192 => 'E_DEPRECATED',
+        2048 => 'E_STRICT',
+        2047 => 'E_ALL',
+        1024 => 'E_USER_NOTICE',
+        512 => 'E_USER_WARNING',
+        256 => 'E_USER_ERROR', // This is a show stopper.
+        128 => 'E_COMPILE_WARNING',
+        64 => 'E_COMPILE_ERROR', // This is a show stopper.
+        32 => 'E_CORE_WARNING',
+        16 => 'E_CORE_ERROR',
+        8 => 'E_NOTICE',
+        4 => 'E_PARSE', // This is a show stopper.
+        2 => 'E_WARNING',
+        1 => 'E_ERROR' // This is a show stopper.
+    );
+    $result = '';
+    foreach ($errorlevels as $number => $name) {
+        if (($intval & $number) == $number) {
+            $result .= ($result != '' ? $separator : '') . $name;
+        }
+    }
+    return $result;
+};
+
+/**
+ * Logs fatal errors and returns an HTTP 500 response code.
+ */
+register_shutdown_function(function() use ($describeErrorReporting, $fatalErrorCodeArray) {
+    $last_error = error_get_last();
+    // If the type was a FATAL-ish error, handle it elegantly.
+    if (is_array($last_error) && in_array($last_error['type'], $fatalErrorCodeArray)) {
+        ob_end_clean();
+        echo "\nFatal PHP error encountered: \n";
+        print_r($last_error);
+    }
+});
